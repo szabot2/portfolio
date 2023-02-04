9f680b071277fa563886b5ca2c21475af57e2941
tomikaka22 <szabotamastibor@tutanota.com>
2023-02-04 23:52:34 +0100

    Főoldal.

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,2 +1,16 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+<script>
+	import { base } from "$app/paths";
+ </script>
+
+<main class="w-screen h-screen">
+	<h1 class="text-white text-center text-xl my-1">Szabó Tamás Tibor portfóliója</h1>
+
+	<div class="grid grid-cols-7 mx-auto justify-items-center gap-y-5">
+
+		<a href="{base}/tesla" class="w-40 group">
+			<p class="text-center text-white m-2 font-semibold group-hover:text-amber-600 transition duration-300"> Mini Tesla tekercs</p>
+			<img class="rounded-3xl outline outline-2 outline-white group-hover:outline-amber-600 transition-all duration-300" src="tesla2.jpg" alt="" />
+		</a>
+
+	</div>
+</main>
