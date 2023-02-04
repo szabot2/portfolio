c8e29e1f77d5625ec57f3f29c357a2e1576cd4a0
tomikaka22 <szabotamastibor@tutanota.com>
2023-02-04 23:01:34 +0100

    Updates

--- _app/immutable/chunks/3-6e6238fc.js
+++
@@ -1,1 +0,0 @@
-import{default as t}from"../components/pages/tesla/_page.svelte-697ca026.js";export{t as component};
---
+++ _app/immutable/chunks/3-c145f4e0.js
@@ -0,0 +1,1 @@
+import{default as t}from"../components/pages/tesla/_page.svelte-12a29a8f.js";export{t as component};
