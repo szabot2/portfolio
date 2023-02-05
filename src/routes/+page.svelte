dc40cfe828cbda8622f95291242a7f0b2022e804
tomikaka22 <szabotamastibor@tutanota.com>
2023-02-05 15:43:18 +0100

    Astabil Multivibrator.

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -8,10 +8,14 @@
 	<div class="flex gap-8 flex-wrap justify-center mb-4">
 
 		<a href="{base}/tesla" class="w-40 group">
-			<p class="text-center text-white m-2 font-semibold group-hover:text-amber-600 transition duration-300"> Mini Tesla tekercs</p>
+			<p class="text-center text-white my-2 font-semibold group-hover:text-amber-600 transition duration-300">Mini Tesla tekercs</p>
 			<img class="rounded-3xl outline outline-2 outline-white group-hover:outline-amber-600 transition-all duration-300" src="{base}/tesla/tesla2.jpg" alt="" />
 		</a>
 
+		<a href="{base}/multivibrator" class="w-40 group">
+			<p class="text-center text-white my-2 font-semibold group-hover:text-amber-600 transition duration-300">Astabil multivibrátor</p>
+			<img class="rounded-3xl outline outline-2 outline-white group-hover:outline-amber-600 transition-all duration-300" src="{base}/multivibrator/multivibrator90.jpg" alt="" />
+		</a>
 
 	</div>
 </main>
---
+++ src/routes/multivibrator/+page.svelte
@@ -0,0 +1,60 @@
+<main class="font-serif">
+	<div class="bg-neutral-800 m-4 mb-0 rounded-3xl p-7 text-white">
+		A billenőkörök pozitívan visszacsatolt univerzális digitális áramkörök,
+		melyeket négyszögjelek előállítására használunk. Kimeneti feszültségük nem
+		folytonosan változik, hanem két, az áramköri paraméterek által
+		meghatározott értéket vehet fel. Az egyes állapotok közötti átbillenés
+		több különböző módon is történhet; ezek szerint létezik:
+		<ul>
+			<li>• bistabil multivibrátor --- mint a neve is mutatja, mindkét állapota stabil. A kimeneti
+				 jelszint csak akkor változik, ha az átbillenési folyamatot egy bemeneti jel kiváltja.</li>
+			<li>• monostabil multivibrátor --- csak egy stabil állapota van. A másik (instabil) állapotát egy
+				 bemeneti jellel válthatjuk ki, és az csak az alkatrészek értékei által meghatározott ideig
+				 marad fenn. Ezen idő eltelte után az áramkör automatikusan visszabillen a stabil
+				 állapotába.</li>
+			<li>• astabil multivibrátor --- nincs stabil állapota. Külső vezérlés nélkül, periodikusan
+				 változtatja kimeneti feszültségszintjét, „billeg” a két állapota között</li>
+	  </ul>
+  
+	</div>
+
+	<h1 class="text-center my-8 text-white text-4xl font-semibold">Astabil Multivibrátor</h1>
+
+	<div class="grid grid-cols-2 max-w-screen-lg mx-auto bg-neutral-800 p-6 rounded-3xl gap-8">
+		<div>
+			<img class="max-h-full max-w-full rounded-2xl" src="/multivibrator/multivibrator.jpg" alt="" />
+		</div>
+		<div>
+			<img class="max-h-full max-w-full rounded-2xl" src="multivibrator/multivibrator.apng" alt="">
+		</div>
+	</div>
+
+	<div class="grid gap-8 grid-flow-col text-white mt-16 mb-10 items-center max-w-fit mx-auto">
+
+		<div class="bg-neutral-800 rounded-3xl p-4">
+
+			<h2 class="text-center text-2xl font-semibold">Alkatrészek:</h2>
+			<div class="text-center p-4 border-b border-solid border-white border-opacity-20">4db ellenállás</div>
+			<div class="text-center p-4 border-b border-solid border-white border-opacity-20">2db Kondenzátor</div>
+			<div class="text-center p-4 border-b border-solid border-white border-opacity-20">2db transistor</div>
+			<div class="text-center pt-4">2db LED</div>
+			
+		</div>
+
+		<div class="bg-neutral-800 rounded-3xl p-4">
+
+			<h2 class="text-center text-2xl font-semibold">Alkatrészek adatai</h2>
+			<div class="text-center p-4 border-b border-solid border-white border-opacity-20">R1 = 675 ohm</div>
+			<div class="text-center p-4 border-b border-solid border-white border-opacity-20">R2 = 670 ohm</div>
+			<div class="text-center p-4 border-b border-solid border-white border-opacity-20">R3 = 29.62 ohm</div>
+			<div class="text-center p-4 border-b border-solid border-white border-opacity-20">R4 = 29.52 ohm</div>
+			<div class="text-center pt-4 ">2* 47 microfarad</div>
+		</div>
+
+	</div>
+
+	<h1 class="text-2xl font-semibold text-center text-white">Kapcsolási rajz</h1>
+	<div class="bg-neutral-800 rounded-3xl p-4 max-w-fit mx-auto mb-5">
+		<img class="rounded-3xl w-96" src="/multivibrator/astabilRajz.png" alt="">
+	</div>
+</main>
