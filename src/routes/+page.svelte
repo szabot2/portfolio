32977e771726194eddfd611b32a795829289aea4
tomikaka22 <szabotamastibor@tutanota.com>
2023-02-05 12:19:44 +0100

    Fooldal responsive

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -3,14 +3,15 @@
  </script>
 
 <main class="w-screen h-screen">
-	<h1 class="text-white text-center text-xl my-1">Szabó Tamás Tibor portfóliója</h1>
+	<h1 class="text-white text-center text-xl my-2">Szabó Tamás Tibor portfóliója</h1>
 
-	<div class="grid grid-cols-7 mx-auto justify-items-center gap-y-5">
+	<div class="flex gap-8 flex-wrap justify-center mb-4">
 
 		<a href="{base}/tesla" class="w-40 group">
 			<p class="text-center text-white m-2 font-semibold group-hover:text-amber-600 transition duration-300"> Mini Tesla tekercs</p>
 			<img class="rounded-3xl outline outline-2 outline-white group-hover:outline-amber-600 transition-all duration-300" src="{base}/tesla2.jpg" alt="" />
 		</a>
+
 
 	</div>
 </main>
