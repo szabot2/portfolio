c395d084d28aab4d2c3b203a05c13b21ef4d185b
szabot2 <szabot2@kkszki.hu>
2023-02-17 15:50:34 +0100

    absolute navbar positioning.

--- src/lib/components/Navbar.svelte
+++ src/lib/components/Navbar.svelte
@@ -2,6 +2,6 @@
 	import { base } from '$app/paths';
 </script>
 
-<div class="bg-[var(--background-color)] rounded-b-full p-4 px-10 fixed top-0 w-full h-16">
+<div class="bg-[var(--background-color)] rounded-b-full p-4 px-10 absolute top-0 w-full h-16">
 	<a href="{base || '/'}" class="bg-neutral-800 text-white w-fit rounded-full p-2 outline hover:outline-amber-600 hover:text-amber-600 transition-all duration-300 cursor-pointer">Főoldal</a>
 </div>
