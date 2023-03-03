b364d85a8dd554c779c6f401115ce3e6b38131b6
tomikaka22 <szabotamastibor@tutanota.com>
2023-03-03 18:53:20 +0100

    Updates

--- _app/immutable/chunks/1.4ac71414.js
+++
@@ -1,1 +0,0 @@
-import{default as t}from"../entry/error.svelte.049d57af.js";export{t as component};
--- _app/immutable/chunks/2.90a5a18c.js
+++
@@ -1,1 +0,0 @@
-import{default as t}from"../entry/_page.svelte.b38a92e1.js";export{t as component};
