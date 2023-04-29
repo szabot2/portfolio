6619e091596a9a9ca6fe35f36831e2e14248a961
tomikaka22 <szabotamastibor@tutanota.com>
2023-04-29 11:33:23 +0200

    Title scaling fixek.

--- src/lib/components/Title.svelte
+++ src/lib/components/Title.svelte
@@ -3,15 +3,15 @@
 	export let title;
 </script>
 
-<div class="flex justify-center items-center mt-5 md:h-[calc(100vh-4rem)] md:mt-0">
+<div class="flex justify-center items-center mx-8 mt-8 md:mx-16 md:h-[calc(100vh-4rem)] md:mt-0">
 	<div class="max-w-screen-2xl">
-		<div class="flex justify-center items-center px-5">
-			<div class="z-10 mx-5 absolute text-primary font-bold text-3xl sm:text-4xl md:text-7xl lg:text-8xl xl:text-9xl text-center">
+		<div class="flex justify-center items-center relative">
+			<div class="z-10 px-3 absolute text-primary font-bold text-3xl sm:text-4xl md:px-5 md:text-7xl lg:text-8xl xl:text-9xl 2xl:text-[9rem] text-center">
 				<h1>{title}</h1>
 			</div>
 	
-			<div class="brightness-50 max-h-[80vh] rounded-full overflow-hidden">
-				<img class="w-full h-full" src={image} alt="">
+			<div class="brightness-50 min-w-[calc(100vw-8rem)] md:h-[calc(100vh-4rem-8rem)] rounded-full overflow-hidden">
+				<img class="w-full h-full object-cover" src={image} alt="">
 			</div>
 		</div>
 	</div>
