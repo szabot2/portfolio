538a9f513c309a16732028126f73d5599ea050a1
tomikaka22 <szabotamastibor@tutanota.com>
2023-05-06 17:16:19 +0200

    title mobil scaling optimalizácoó.

--- src/lib/components/Title.svelte
+++ src/lib/components/Title.svelte
@@ -3,14 +3,14 @@
 	export let title;
 </script>
 
-<div class="flex justify-center items-center mt-8 md:mx-16 md:h-[calc(100vh-4rem)] md:mt-0">
-	<div class="max-w-screen-2xl">
+<div class="flex justify-center items-center md:mx-16 h-[calc(100vh-4rem)] mt-0">
+	<div>
 		<div class="flex justify-center items-center relative">
-			<div class="z-10 px-3 absolute text-primary font-bold text-3xl sm:text-4xl md:px-5 md:text-7xl lg:text-8xl xl:text-9xl 2xl:text-[9rem] text-center">
+			<div class="z-10 leading-[1.2] px-10 absolute text-primary font-bold text-6xl md:px-5 md:text-7xl lg:text-8xl xl:text-9xl 2xl:text-[9rem] text-center">
 				<h1>{title}</h1>
 			</div>
 	
-			<div class="brightness-50 min-w-[calc(100vw-8rem)] min-h-[6rem] md:h-[calc(100vh-4rem-8rem)] rounded-full overflow-hidden">
+			<div class="brightness-50 min-w-[calc(100vw-8rem)] h-[calc(100vh-4rem-8rem)] md:h-[calc(100vh-4rem-8rem)] rounded-full overflow-hidden">
 				<img class="w-full h-full object-cover" src={image} alt="">
 			</div>
 		</div>
