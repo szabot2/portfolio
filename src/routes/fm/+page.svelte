08e6c2a48af4cb060ee70b7600d59ecba91791f7
tomikaka22 <szabotamastibor@tutanota.com>
2024-04-09 15:32:09 +0200

    FM update.

--- src/routes/fm/+page.svelte
+++ src/routes/fm/+page.svelte
@@ -6,17 +6,33 @@
 
 <main>
 
-	<Title title={'Primitív Frekvencia Modulációs Rádió'} image={'fm/fm2.webp'}></Title>
+	<Title title={'Primitív Frekvencia Modulációs Rádió'} image={'/fm/fm2.webp'}></Title>
 
 	<h1 data-aos="fade-down" class="text-primary font-semibold text-3xl text-center">Az áramkör:</h1>
-	<a data-aos="fade-down" href="fm/kapcs.webp">
-		<img class="w-full mx-auto rounded-3xl lg:w-8/12 xl:w-7/12" src="fm/kapcs.webp" alt="">
-	</a>
+	<img data-aos="fade-down" class="w-full mx-auto lg:w-8/12 xl:w-7/12" src="/fm/kapcs.webp" alt="">
 
-    <div>
-        <p class="text-secondary text-center text-lg font-semibold">
-            Lorem ipsum dolor sit amet consectetur adipisicing elit. Quisquam molestias delectus doloremque recusandae nemo illo minus aut omnis natus odio.
-        </p>
-    </div>
+	<div class="flex justify-center my-20">
+		<div class="flex text-onSecondaryContainer text-2xl font-semibold justify-center items-center bg-secondaryContainer p-6 w-8/12 rounded-full">
+			<img src="/fm/trans.webp" alt="">
+			<img src="/fm/LC.webp" alt="">
+			<p class="ml-10">Az áramkör lényegében egy LC rezonátorból, és egy tranzisztorból áll.</p>
+		</div>
+	</div>
+
+	<div class="flex justify-center my-20 mx-24">
+		<div class="flex text-secondary text-2xl font-semibold justify-center items-center w-10/12">
+			<img src="/fm/divider.webp" alt="">
+			<p class="ml-14">Az R1, R2 és R3 ellenállások segítségével beállítjuk a tranzisztor munkapontját.</p>
+		</div>
+	</div>
+
+	<div class="flex justify-center my-20 mx-24">
+		<div class="flex text-secondary text-2xl font-semibold justify-center items-center w-10/12">
+			<img src="/fm/inout.webp" alt="">
+			<p class="ml-14">Ha az IN és OUT pontokra egy váltakozó feszültségű forrást kapcsolunk (mikrofon, aux kábel, stb.) a tranzisztor annak függvényében növeli és csökkenti az RC körön átfolyó áramot.</p>
+		</div>
+	</div>
+
+	<p class="text-primary text-2xl font-semibold text-center mb-20">Ennek hatására a rezgőkör megváltoztatja a frekvenciáját, így belemoduláltuk a frekvenciájába a beérkező jelünket.</p>
 
 </main>
